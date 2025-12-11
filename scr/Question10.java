25776a0e757c714e1c150c251d0efa5285327229
Suyogya13 <tiwarisuyogya@gmail.com>
2025-12-11 14:22:04 +0530

    Implement binary search

---
+++ scr/Question10.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Question10 {
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        System.out.println("enter the target element");
+        int target = sc.nextInt();
+
+        int index = binarySearch(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+}
