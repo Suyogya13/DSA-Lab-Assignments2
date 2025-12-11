f1a8c9f6d506b83197fa04509536453debc85d0c
Suyogya13 <tiwarisuyogya@gmail.com>
2025-12-11 14:19:00 +0530

    Implement Tower of Hanoi algorithm

---
+++ scr/Question7.java
@@ -0,0 +1,18 @@
+public class Question7 {
+
+    public static void towerOfHanoi(int n, char fromRod, char toRod, char auxRod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + fromRod + " to " + toRod);
+            return;
+        }
+        towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+        System.out.println("Move disk " + n + " from " + fromRod + " to " + toRod);
+
+        towerOfHanoi(n - 1, auxRod, toRod, fromRod);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }
+}
